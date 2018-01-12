061d9abb3fe2539efac934b3c6ed523aa7cb8d7a
joicepj11 <joicepj11@gmail.com>
2018-01-12 12:56:07 +0530

    1 single instances of Gson , Retrofit, OkHttpClient. 2 added converter factories to retrofit to handle response.

--- app/src/main/java/com/sethu/androidtemplatecode/MainActivity.java
+++ app/src/main/java/com/sethu/androidtemplatecode/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sethu.androidtemplatecode;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.sethu.androidtemplatecode.view.RetrofitDemoActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -80,18 +83,10 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        }else if(id == R.id.retrofit){
+            startActivity(new Intent(this, RetrofitDemoActivity.class));
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
--- app/src/main/java/com/sethu/androidtemplatecode/module/NetworkModule.java
+++ app/src/main/java/com/sethu/androidtemplatecode/module/NetworkModule.java
@@ -3,6 +3,8 @@ package com.sethu.androidtemplatecode.module;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.sethu.androidtemplatecode.model.EmptyToNullConverter;
+import com.sethu.androidtemplatecode.model.UserEnvelopConverter;
 
 import dagger.Module;
 import dagger.Provides;
@@ -10,29 +12,40 @@ import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+
 @Module
 public class NetworkModule {
-
+    OkHttpClient client;
+    GsonBuilder gsonBuilder;
+    Retrofit retrofit;
     @Provides
     Gson provideGson() {
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+       if(gsonBuilder == null) {
+           gsonBuilder = new GsonBuilder();
+           gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+       }
         return gsonBuilder.create();
     }
 
     @Provides
     OkHttpClient provideOkHttpClient() {
-        OkHttpClient client = new OkHttpClient();
+        if(client == null) {
+            client = new OkHttpClient();
+        }
         return client;
     }
 
     @Provides
     Retrofit provideRetrofit(Gson gson, OkHttpClient okHttpClient) {
-        Retrofit retrofit = new Retrofit.Builder()
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .baseUrl("https://reqres.in")
-                .client(okHttpClient)
-                .build();
+        if(retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .addConverterFactory(new EmptyToNullConverter())
+                    .addConverterFactory(new UserEnvelopConverter())
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .baseUrl("https://reqres.in")
+                    .client(okHttpClient)
+                    .build();
+        }
         return retrofit;
     }
 }
