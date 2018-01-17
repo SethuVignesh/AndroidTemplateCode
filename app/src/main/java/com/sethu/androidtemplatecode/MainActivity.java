1bb0e694119ffc404dd1fb1f9120502d0eb2cf80
sethu <sethu.store@gmail.com>
2018-01-17 11:59:56 +0530

    refactor: moved if else to switch case

--- app/src/main/java/com/sethu/androidtemplatecode/MainActivity.java
+++ app/src/main/java/com/sethu/androidtemplatecode/MainActivity.java
@@ -3,9 +3,8 @@ package com.sethu.androidtemplatecode;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.sethu.androidtemplatecode.view.RetrofitDemoActivity;
 
@@ -83,11 +83,16 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.camera) {
-            // Handle the camera action
-        }else if(id == R.id.retrofit){
-            startActivity(new Intent(this, RetrofitDemoActivity.class));
+        switch (id) {
+            case R.id.camera:
+                break;
+            case R.id.retrofit:
+                startActivity(new Intent(this, RetrofitDemoActivity.class));
+                break;
+            case R.id.lru_cache:
+                break;
         }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
